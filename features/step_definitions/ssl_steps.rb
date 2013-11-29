f5978470159591848923df1318ad1bb9da3aa301
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2013-11-29 12:14:09 +0000

    remove spurious 443s
    these try to analyze an additional host called "443" which fails.

--- features/step_definitions/ssl_steps.rb
+++ features/step_definitions/ssl_steps.rb
@@ -3,16 +3,16 @@ Before do
 end
 
 When(/^we test using the "(.*?)" protocol$/) do |protocol|
-  step("I run `sslyze.py --#{protocol} #{ENV['HOST']} 443`")
+  step("I run `sslyze.py --#{protocol} #{ENV['HOST']}`")
   step("the exit status should be 0")
 end
 
 When(/^we check the certificate$/) do
-  step("I run `sslyze.py --certinfo=basic #{ENV['HOST']} 443`")
+  step("I run `sslyze.py --certinfo=basic #{ENV['HOST']}`")
   step("the exit status should be 0")
 end
 
 When(/^we test certificate renegotiation$/) do
-  step("I run `sslyze.py --reneg #{ENV['HOST']} 443`")
+  step("I run `sslyze.py --reneg #{ENV['HOST']}`")
   step("the exit status should be 0")
 end
